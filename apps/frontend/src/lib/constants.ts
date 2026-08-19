f02cd9c5df921fbfc5f0ae517b33cf1dd2d719c2
Huynh Ky <huynhbaoky12369@gmail.com>
2026-08-19 15:06:24 +0700

    sua NEXT_PUBLIC_BACKEND_URL

--- apps/frontend/src/lib/constants.ts
+++ apps/frontend/src/lib/constants.ts
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 
 export const DEFAULT_PAGE_SIZE = 12;
 export const DEFAULT_POSTS_SIZE = 6;
